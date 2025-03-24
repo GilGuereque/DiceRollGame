fe7e32cd9bcac435ed15ac5f6e32b2679050f2df
Gil Guereque <gguereque326@gmail.com>
2025-03-24 11:26:56 -0700

    Refactoring Diceroll app flow

--- DiceRollGame/Program.cs
+++ DiceRollGame/Program.cs
@@ -1,12 +1,45 @@
 ﻿var diceNumbers = new RandonNumberGenerator();
 
-//while (userGuess <> diceNumber && userTries <= 3)
+//while (userGuess<> diceNumber && userTries <= 3)
 
+
+class NumberGuesser
+{
+    public int userTries = 0;
+    public string? userGuess { get; private set; }
+
+    public Random random = new Random();
+
+    public int ranNum = 0;
+    public NumberGuesser()
+    {
+        while (userTries < 3)
+        {
+            Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries.");
+            bool v = int.TryParse(Console.ReadLine(), out int userInt);
+            ranNum = random.Next(1, 6);
+            if (userInt == ranNum)
+            {
+                Console.WriteLine("You win.");
+                break;
+            }
+            else if (userInt != ranNum);
+            {
+                Console.WriteLine("Wrong number");
+                continue;
+            }
+            userTries++;
+            if (userTries == 3)
+            {
+                break;
+            }
+        }
+    }
+}
 
 class RandonNumberGenerator
 {
-    public int diceNumber { get; private set; }
-    public int userGuess { get; private set; }
+    public int? diceNumber { get; private set; }
 
     Random random = new Random();
 
--- DiceRollGame/obj/Debug/net8.0/DiceRollGame.AssemblyInfo.cs
+++ DiceRollGame/obj/Debug/net8.0/DiceRollGame.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b10e51b96da2f481c0d39318f7aac41c9dff1a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc39616f08e1b011073874e30912bec241db65e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DiceRollGame")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
