4c1d3d0facd9d17fbda689d3925929f94a39a345
Gil Guereque <gguereque326@gmail.com>
2025-03-24 16:02:00 -0700

    Added user input validation and a few comments to make the program flow more descriptive

--- DiceRollGame/Program.cs
+++ DiceRollGame/Program.cs
@@ -20,27 +20,36 @@ class NumberGuesser
         while (userTries < 3)
         {
             Console.WriteLine("\nDice rolled. Guess what number it shows in 3 tries. (Number must be from 1 to 6).");
-            bool v = int.TryParse(Console.ReadLine(), out int userInt);
-            ranNum = random.Next(1, 6);
+            bool validInput = int.TryParse(Console.ReadLine(), out int userInt);
+            ranNum = random.Next(1, 7);
+
+            // Validate the user input and ensure it is a numerical value between 1 & 6
+            if (!validInput || userInt < 1 ||  userInt > 6)
+            {
+                Console.WriteLine("Sorry, please enter a valid number between 1 & 6. Try again.");
+                continue; // Skip to the next iteration if parsing fails or the value is out of range
+            }
+
+            // Check the users guess against the random number generated
             if (userInt == ranNum)
             {
                 userTries++;
                 Console.WriteLine("\nYou win.");
                 Console.WriteLine("Press enter to exit game.");
                 Console.ReadKey();
-                return;
+                return; // End the method if the user wins
             }
             else if (userInt != ranNum)
             {
                 userTries++;
                 Console.WriteLine($"\nWrong number. The dice rolled was {ranNum}.");
-                //continue;
             }
 
         }
     }
 }
 
+// Not neccessary
 class RandonNumberGenerator
 {
     public int? diceNumber { get; private set; }
