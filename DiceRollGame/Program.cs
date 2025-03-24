994526bd52d7d842e699284fd9fdca00f020f05d
Gil Guereque <gguereque326@gmail.com>
2025-03-24 12:17:49 -0700

    Refactoring program

--- DiceRollGame/Program.cs
+++ DiceRollGame/Program.cs
@@ -1,40 +1,42 @@
-﻿//var diceNumbers = new RandonNumberGenerator();
-
-//while (userGuess<> diceNumber && userTries <= 3)
+﻿// Instantiate Dice roll game object
 var DiceRollGuesser = new NumberGuesser();
 DiceRollGuesser.DiceRoll();
+
+// Pause game before exiting
+Console.WriteLine("Game over. Press enter to exit program.");
+Console.ReadKey();
 
 
 class NumberGuesser
 {
     public int userTries = 0;
-    public string? userGuess { get; private set; }
+    //public string? userGuess { get; private set; }
 
     public Random random = new Random();
 
     public int ranNum = 0;
     public void DiceRoll()
     {
-        while (userTries <= 3)
+        while (userTries < 3)
         {
-            Console.WriteLine("Dice rolled. Guess what number it shows in 3 tries. (Number must be from 1 to 6).");
+            Console.WriteLine("\nDice rolled. Guess what number it shows in 3 tries. (Number must be from 1 to 6).");
             bool v = int.TryParse(Console.ReadLine(), out int userInt);
             ranNum = random.Next(1, 6);
             if (userInt == ranNum)
             {
+                userTries++;
                 Console.WriteLine("You win.");
-                break;
+                Console.WriteLine("Press enter to exit program.");
+                Console.ReadKey();
+                return;
             }
-            else if (userInt != ranNum);
+            else if (userInt != ranNum)
             {
-                Console.WriteLine("Wrong number");
-                continue;
+                userTries++;
+                Console.WriteLine($"Wrong number. The dice roll was {ranNum}. Guess again:");
+                //continue;
             }
-            userTries++;
-            if (userTries == 3)
-            {
-                break;
-            }
+
         }
     }
 }
