076466e79447318ea75186b54332f1369efe56f0
Gil Guereque <gguereque326@gmail.com>
2025-03-27 09:34:31 -0700

    Added an alternate solution at the bottom of the file

--- DiceRollGame/Program.cs
+++ DiceRollGame/Program.cs
@@ -61,3 +61,81 @@ class RandonNumberGenerator
         diceNumber = random.Next(1, 6);
     }
 }
+
+
+// Alternative Solution
+var random = new Random();
+var dice = new Dice(random);
+var guessingGame = new GuessingGame(dice);
+
+bool xxx= guessingGame.Play();
+//var diceRollResult = random.Next(1, 7);
+//Console.WriteLine(diceRollResult);
+
+Console.ReadKey();
+
+class GuessingGame
+{
+    private readonly Dice _dice;
+    private const int InitialTries = 3;
+
+    public GuessingGame(Dice dice)
+    {
+        _dice = dice;
+    }
+
+    public bool Play()
+    { 
+        var diceRollResult = _dice.Roll();
+        Console.WriteLine(
+            $"Dice rolled. Guess what number it shows in {InitialTries} tries.");
+
+        var triesLeft = InitialTries;
+        while(triesLeft > 0)
+        {
+            var guess = ConsoleReader.ReadInteger("Enter a number:");
+            if (guess == diceRollResult)
+            {
+                return true;
+            }
+            Console.WriteLine("Wrong number.");
+            --triesLeft;
+        }
+        return false;
+    }
+}
+
+public static class ConsoleReader
+{
+    public static int ReadInteger(string message)
+    {
+        int result;
+        do
+        {
+            Console.WriteLine(message);
+        }
+        while (!int.TryParse(Console.ReadLine(), out result));
+        return result;
+    }
+}
+
+public class Dice
+{
+
+    private Random _random = new Random();
+
+    private const int SidesCount = 6; // to account for different sided die
+
+public Dice(Random random)
+{
+    _random = random;
+}
+
+public int Roll()
+{
+    return _random.Next(1, SidesCount + 1);
+}
+
+public void Describe() =>
+    Console.WriteLine($"This is a dice with 6 sides");
+}
